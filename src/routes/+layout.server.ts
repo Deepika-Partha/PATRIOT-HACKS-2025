9f777f154eaf6a4f0acd773e44c26d5b263c73ca
Neil Raji John <neilrjohn@Neils-MacBook-Air-2.local>
2025-11-15 20:08:44 -0500

    Schedule

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,19 +1,19 @@
-import { redirect } from '@sveltejs/kit';
-import { verifyJwt } from '$lib/server/jwt';
-import type { RequestEvent } from '@sveltejs/kit';
+// import { redirect } from '@sveltejs/kit';
+// import { verifyJwt } from '$lib/server/jwt';
+// import type { RequestEvent } from '@sveltejs/kit';
 
 
-export async function POST({ request, cookies }: RequestEvent) {
-    const token = cookies.get('jwt');
-    const user: JwtPayload | null = token ? verifyJwt(token) : null;
+// export async function POST({ request, cookies }: RequestEvent) {
+//     const token = cookies.get('jwt');
+//     const user: JwtPayload | null = token ? verifyJwt(token) : null;
 
-    // Redirect to login if user not authenticated and not already on login page
-    if (!user && url.pathname !== '/login') {
-        throw redirect(302, '/login');
-    }
+//     // Redirect to login if user not authenticated and not already on login page
+//     if (!user && url.pathname !== '/login') {
+//         throw redirect(302, '/login');
+//     }
 
-    return { user };
-};
+//     return { user };
+// };
 
 
 
